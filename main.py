53885a0e882a5cd428b1b1030585108a47afb7c1
Kamil <matysekkamil@wp.pl>
2021-04-18 22:56:53 +0200

    zadanie domowe

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+import hashlib
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+from datetime import datetime
+from pydantic import BaseModel
+from datetime import timedelta
 
 app = FastAPI()
+app.counter = 0
+app.dicti = {}
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/method")
+def root():
+    return {"method": "GET"}
+    
+@app.put("/method")
+def root():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def root():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method")
+def root():
+    return {"method": "DELETE"}
+
+@app.post("/method", status_code=201)
+def root():
+    return {"method": "POST"}
+
+@app.post("/method")
+def root():
+    return {"method": "POST"}
+
+@app.get("/auth", status_code=204)
+def root(password: str, password_hash: str, response: Response):
+    pass_hash = hashlib.sha512( password.encode("utf-8") ).hexdigest()
+    if pass_hash != password_hash:
+        response.status_code = 401
+    else:
+        response.status_code = 204
+
+class Item(BaseModel):
+    name: str
+    surname:str
+
+@app.post("/register", status_code=201)
+def root(item: Item):
+    app.counter +=1
+    register_date = datetime.today()
+    json= {
+        "id": app.counter,
+        "name": item.name,
+        "surname": item.surname,
+        "register_date": register_date.strftime('%Y-%m-%d'),
+        "vaccination_date": (register_date + timedelta(days=(len(item.name))+(len(item.surname)))).strftime('%Y-%m-%d')
+        }
+    app.dicti[app.counter]=json
+    return json
+
+@app.get("/patient/{id}", status_code=200)
+def root(id: int, response: Response):
+    if id<1:
+        response.status_code = 400
+    else:
+        if id in app.dicti.keys():
+            return app.dicti[id]
+        else:
+            response.status_code = 404
+
+    
