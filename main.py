d32c20ec1b3787c505c82cd653b5044de538ca03
Kamil <matysekkamil@wp.pl>
2021-04-18 23:54:57 +0200

    zadanie domowe

--- main.py
+++ main.py
@@ -39,9 +39,11 @@ def root():
     return {"method": "POST"}
 
 @app.get("/auth", status_code=401)
-def root(password, password_hash, response: Response):
-    pass_hash = hashlib.sha512( str(password).encode("utf-8") ).hexdigest()
-    if pass_hash != str(password_hash):
+def root(password: str, password_hash: str, response: Response):
+    pass_hash = hashlib.sha512( password.encode("utf-8") ).hexdigest()
+    if pass_hash != password_hash:
+        response.status_code = 401
+    elif not password:
         response.status_code = 401
     else:
         response.status_code = 204
@@ -54,23 +56,15 @@ class Item(BaseModel):
 def root(item: Item):
     app.counter +=1
     register_date = datetime.today()
-    letters=0
-    for i in item.name:
-        if item.name[i].isalpha():
-            letters += 1
-    for i in item.surname:
-        if item.surname[i].isalpha():
-            letters += 1
     json= {
         "id": app.counter,
         "name": item.name,
         "surname": item.surname,
         "register_date": register_date.strftime('%Y-%m-%d'),
-        "vaccination_date": (register_date + timedelta(days=letters)).strftime('%Y-%m-%d')
+        "vaccination_date": (register_date + timedelta(days=(len(item.name))+(len(item.surname)))).strftime('%Y-%m-%d')
         }
-    app_json = json.dumps(json)
     app.dicti[app.counter]=json
-    return app_json
+    return json
 
 @app.get("/patient/{id}", status_code=200)
 def root(id: int, response: Response):
@@ -78,7 +72,6 @@ def root(id: int, response: Response):
         response.status_code = 400
     else:
         if id in app.dicti.keys():
-            response.status_code = 200
             return app.dicti[id]
         else:
             response.status_code = 404
